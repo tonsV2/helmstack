7b8e374140263695ba31cfd26c77184068bbabcb
sj <sebastianthegreatful@gmail.com>
2019-02-16 21:26:24 +0100

    Add description

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='helmstack',
     version='1.0.0',
+    description='See https://github.com/tonsV2/helmstack',
     py_modules=['helmstack'],
     install_requires=[
         'click',
