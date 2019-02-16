c127a61080846eef5e39d09de8b068f01a69dd08
sj <sebastianthegreatful@gmail.com>
2019-02-16 10:38:57 +0100

    Print commands for adding repositories

--- helmstack.py
+++ helmstack.py
@@ -16,6 +16,17 @@ def cli(environment, file, debug):
         stack = yaml.safe_load(file)
         if debug:
             pprint.pprint(stack)
-
+        handle_repositories(stack)
     except yaml.YAMLError as exc:
         print(exc)
+
+
+def handle_repositories(stack):
+    if stack['repositories']:
+        for repository in stack['repositories']:
+            helm("helm repo add %s %s" % (repository['name'], repository['url']))
+        helm("helm repo update")
+
+
+def helm(cmd):
+    print(cmd)
