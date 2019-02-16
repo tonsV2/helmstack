f7babbd5ca73abbde14d7ad9aecd3b20be8e0a27
sj <sebastianthegreatful@gmail.com>
2019-02-16 19:47:39 +0100

    s/hello/helmstack/

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='helmstack',
     version='1.0.0',
-    py_modules=['hello'],
+    py_modules=['helmstack'],
     install_requires=[
         'click',
         'ruamel.yaml'
