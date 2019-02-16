e1317c1bf7d330f8acf75952af0eb8ab43f33932
sj <sebastianthegreatful@gmail.com>
2019-02-16 21:46:46 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='helmstack',
-    version='1.0.0',
+    version='1.1.0',
     description='See https://github.com/tonsV2/helmstack',
     py_modules=['helmstack'],
     install_requires=[
