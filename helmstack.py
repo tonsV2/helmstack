9f4b03c532c9ae8449fbf4f9a1dad9bba0efa626
sj <sebastianthegreatful@gmail.com>
2019-02-16 08:47:37 +0100

    Only print stack if debug is enabled

--- helmstack.py
+++ helmstack.py
@@ -6,14 +6,17 @@ import pprint
 @click.command()
 @click.option('--environment', '-e', default=None, help='Specify target environment')
 @click.option('--file', '-f', default='helmstack.yaml', help='Specify stack file')
-def cli(environment, file):
+@click.option('--debug', default=False, help='Enable debug')
+def cli(environment, file, debug):
+    """This script run helm commands"""
     print("Environment: %s" % environment)
     print("Stack file: %s" % file)
 
     with open(file, 'r') as stream:
         try:
             stack = yaml.safe_load(stream)
-            pprint.pprint(stack)
+            if debug:
+                pprint.pprint(stack)
 
         except yaml.YAMLError as exc:
             print(exc)
