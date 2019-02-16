8b5802b7b4dc2c13bdd11a2a3a90ba78cecc36a4
sj <sebastianthegreatful@gmail.com>
2019-02-16 09:17:51 +0100

    Take advantage of click's build in file handling

--- helmstack.py
+++ helmstack.py
@@ -5,18 +5,17 @@ import pprint
 
 @click.command()
 @click.option('--environment', '-e', default=None, help='Specify target environment')
-@click.option('--file', '-f', default='helmstack.yaml', help='Specify stack file')
+@click.option('--file', '-f', type=click.File('r'), default='helmstack.yaml', help='Specify stack file')
 @click.option('--debug', default=False, help='Enable debug')
 def cli(environment, file, debug):
     """This script run helm commands"""
     print("Environment: %s" % environment)
-    print("Stack file: %s" % file)
+    print("Stack file: %s" % file.name)
 
-    with open(file, 'r') as stream:
-        try:
-            stack = yaml.safe_load(stream)
-            if debug:
-                pprint.pprint(stack)
+    try:
+        stack = yaml.safe_load(file)
+        if debug:
+            pprint.pprint(stack)
 
-        except yaml.YAMLError as exc:
-            print(exc)
+    except yaml.YAMLError as exc:
+        print(exc)
