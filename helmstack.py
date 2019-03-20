9511759edb3a73ba58283c03163ecee1e60c4126
sj <sebastianthegreatful@gmail.com>
2019-03-20 17:15:36 +0100

    Support inline values via "set"

--- helmstack.py
+++ helmstack.py
@@ -1,7 +1,10 @@
 import collections.abc as collections_abc
+import os
 import pprint
 import subprocess
 import sys
+import tempfile
+from pathlib import Path
 
 import click
 import ruamel.yaml as yaml
@@ -20,6 +23,8 @@ class Config(object):
         self.overlay = None
         self.recreate_pods = False
         self.force = False
+        self.keep_tmp_value_files = False
+        self.garbage_files = []
 
 
 config = Config()
@@ -66,9 +71,6 @@ def cli(environment, context, helm_binary, file, skip_repos, debug, dry_run):
             if 'force' in helm_defaults:
                 config.force = helm_defaults['force']
 
-    if config.environment:
-        merge_overlays()
-
 
 def trim_releases(targets):
     if len(targets):
@@ -82,11 +84,16 @@ def trim_releases(targets):
 
 @cli.command()
 @click.option('--recreate-pods', is_flag=True, help='Recreate pods')
+@click.option('--keep-tmp-value-files', is_flag=True, help='Don\'t clean up tmp value files')
 @click.argument('targets', nargs=-1, default=None)
-def sync(targets, recreate_pods):
+def sync(targets, recreate_pods, keep_tmp_value_files):
     """Synchronise everything listed in the state file"""
 
     config.recreate_pods = recreate_pods
+    config.keep_tmp_value_files = keep_tmp_value_files
+
+    if config.environment:
+        merge_overlays()
 
     trim_releases(targets)
     if not config.skip_repos:
@@ -102,6 +109,9 @@ def sync(targets, recreate_pods):
 @click.argument('targets', nargs=-1, default=None)
 def delete(targets, purge):
     """Delete everything listed in the state file"""
+
+    if config.environment:
+        merge_overlays()
 
     trim_releases(targets)
     for release in config.stack['releases']:
@@ -156,17 +166,41 @@ def merge(releases, overlays):
         for k, v in right.items():
             if isinstance(left.get(k), dict) and isinstance(v, collections_abc.Mapping):
                 left[k] = dict_merge(left[k], v)
-            elif isinstance(left.get(k), list):
-                left[k] = left[k] + v
             else:
                 left[k] = v
         return left
 
+    def merge_values(overlay, release):
+        release_values = []
+        overlay_values = []
+        if 'values' in release:
+            release_values = release['values']
+            del release['values']
+        if 'values' in overlay:
+            overlay_values = overlay['values']
+            del overlay['values']
+        release_values.extend(overlay_values)
+        return release_values
+
     for release in releases:
         name = release['name']
+        values = []
         if name in overlays:
             overlay = overlays[name]
+            values = merge_values(overlay, release)
             dict_merge(release, overlay)
+
+        transform_set_to_file(release, values)
+
+        if values:
+            release['values'] = values
+
+
+def transform_set_to_file(release, values):
+    if 'set' in release:
+        set_file = to_file(release['set'])
+        config.garbage_files.append(set_file)
+        values.append(set_file)
 
 
 def handle_repositories():
@@ -179,6 +213,15 @@ def handle_repositories():
             sh_exec("%s repo add %s %s" % (config.helm_binary, name, url))
             print("Repository added!")
         sh_exec("%s repo update" % config.helm_binary)
+
+
+def to_file(value):
+    fp = tempfile.NamedTemporaryFile(delete=False)
+    if isinstance(value, str):
+        fp.write(bytes(value, encoding='utf8'))
+    else:
+        fp.write(bytes(yaml.dump(value), encoding='utf8'))
+    return fp.name
 
 
 def helm_upgrade(release):
@@ -205,9 +248,19 @@ def helm_upgrade(release):
     cmd += " --install"
     if 'values' in release:
         for value in release['values']:
-            cmd += " --values %s" % value
+            if not Path(value).is_file():
+                exit_with_error("File not found: %s" % value)
+            else:
+                cmd += " --values %s" % value
     print("Upgrading: %s (%s)" % (name, chart))
     sh_exec(cmd)
+    unlink_garbage_files()
+
+
+def unlink_garbage_files():
+    if not config.keep_tmp_value_files:
+        for file in config.garbage_files:
+            os.unlink(file)
 
 
 def sh_exec(cmd):
